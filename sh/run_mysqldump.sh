71362ff2edc8de176d1842711a294cec66424e11
Sho Maruyama <mrsho34@gmail.com>
2020-05-26 17:51:30 +0900

    Bug fix when directory is not exist.

--- sh/run_mysqldump.sh
+++ sh/run_mysqldump.sh
@@ -10,18 +10,18 @@ readonly local DIFF_FILE="/diff/diff_${DB_NAME}_$(date +"%Y-%m-%dT%H%M%SZ").dump
 readonly local DUMP_OPTIONS=""
 readonly local MYSQL_HOST_OPTS="-h ${DB_HOST} -P ${DB_PORT} -u${DB_USER} -p${DB_PASSWORD}"
 
-mysqldump ${MYSQL_HOST_OPTS} -d --single-transaction ${DB_NAME} > "${DUMP_FILE}"
+mysqldump ${MYSQL_HOST_OPTS} -d --single-transaction ${DB_NAME} > ${DUMP_FILE}
 
 echo "Completed dump"
 
-OLD_DUMP_FILE=/repo/${SAVE_PATH}/dump/${DB_NAME}.dump.sql
+OLD_DUMP_FILE="/repo/${SAVE_PATH}/dump/${DB_NAME}.dump.sql"
 
 if [ -e $OLD_DUMP_FILE ]; then
 echo "Old dump file exists."
 echo "Make diff file"
 diff -F "^CREATE" -c -I "Dump completed on" \
 <(cat $OLD_DUMP_FILE | sed "s/\(AUTO_INCREMENT=\)[0-9][0-9]*/\1/") \
-<(cat $DUMP_FILE | sed "s/\(AUTO_INCREMENT=\)[0-9][0-9]*/\1/") >$DIFF_FILE
+<(cat $DUMP_FILE | sed "s/\(AUTO_INCREMENT=\)[0-9][0-9]*/\1/") | tee -a $DIFF_FILE
 
 if [ -s $DIFF_FILE ]; then
 echo "Find diff !!"
--- sh/upload_repository.sh
+++ sh/upload_repository.sh
@@ -6,12 +6,26 @@ echo "Upload Github Repository..."
 
 cd repo
 if [ -n "$(ls /output)" ]; then
+    if [ ! -d ${SAVE_PATH}schemaspy ] ;then
+        echo "make dir on ${SAVE_PATH}schemaspy"
+        mkdir ${SAVE_PATH}schemaspy
+    fi
     cp -R /output/* ${SAVE_PATH}schemaspy
 fi
+
 if [ -n "$(ls /dump)" ]; then
+    if [ ! -d ${SAVE_PATH}dump ] ;then
+        echo "make dir on ${SAVE_PATH}dump"
+        mkdir ${SAVE_PATH}dump
+    fi
     cp -R /dump/* ${SAVE_PATH}dump
 fi
+
 if [ -n "$(ls /diff)" ]; then
+    if [ ! -d ${SAVE_PATH}diff ] ;then
+        echo "make dir on ${SAVE_PATH}diff"
+        mkdir ${SAVE_PATH}diff
+    fi
     cp -R /diff/* ${SAVE_PATH}diff
 fi
 git config --global user.email "$GITHUB_EMAIL"
